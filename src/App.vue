da0a3765094e7119faf0932e29b029ecfe55cbaa
raulberina <tindig.berina@gmail.com>
2019-11-02 16:10:55 +0800

    Adding

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <AsteroidGrid @remove="remove" :asteroids="asteroids" header="Near-Earth Objects" />
+    <FooterDiv />
+  </div>
+</template>
+
+<script>
+import AsteroidGrid from './components/AsteroidGrid.vue'
+import FooterDiv from './components/FooterDiv.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    AsteroidGrid,
+    FooterDiv
+  },
+  data() {
+      return {
+        asteroids: []
+      }
+  },            
+  created: function () {
+      this.fetchAsteroids();
+  },
+  methods: {
+      fetchAsteroids: function () {
+          var apiKey = 'lkgI9to0hRizfzk4xTAxtNTTFkkA4Mtq7y1yW5me';
+          var url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=' + apiKey;
+          axios.get(url)
+              .then(res => {                                       
+                  this.asteroids = res.data.near_earth_objects.slice(0, 10);
+              });
+      },
+      remove: function (index) {
+          this.asteroids.splice(index, 1);            
+      },
+  }
+}
+</script>
+
+<style>
+  [v-cloak] {
+    display: none;
+  }
+</style>
+
---
+++ src/components/FooterDiv.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="footerDiv">
+		<button class="btn btn-primary">
+			Click Me!
+		</button>
+	</div>
+</template>
